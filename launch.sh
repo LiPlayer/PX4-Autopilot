08c6130fdcb8a85a202694d53213b83f30d48bb4
zhaoli <zhaoli.go@gmail.com>
2025-12-16 20:42:57 +0400

    refact: unilidar to gzlidar

--- launch.sh
+++ launch.sh
@@ -16,11 +16,11 @@ MicroXRCEAgent udp4 -p 8888 &
 AGENT_PID=$!
 
 # Start ROS 2 bridge in background
-ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=$(pwd)/Tools/simulation/gz/models/x500_unilidar/bridge_config.yaml &
+ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=$(pwd)/Tools/simulation/gz/models/x500_gzlidar/bridge_config.yaml &
 BRIDGE_PID=$!
 
 # Start PX4
-PX4_SYS_AUTOSTART=4001 PX4_GZ_WORLD=default  PX4_GZ_MODEL_POSE="-5,0,0,0,0,0" PX4_SIM_MODEL=gz_x500_unilidar ./build/px4_sitl_default/bin/px4
+PX4_SYS_AUTOSTART=4001 PX4_GZ_WORLD=default  PX4_GZ_MODEL_POSE="-5,0,0,0,0,0" PX4_SIM_MODEL=gz_x500_gzlidar ./build/px4_sitl_default/bin/px4
 
 # Cleanup when PX4 exits normally
 cleanup
